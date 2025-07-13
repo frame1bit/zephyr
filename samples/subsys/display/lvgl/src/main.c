c775db13c5b2ee53944f947b00744079b39b7c46
frame1bit <framexbit@gmail.com>
2025-07-14 06:57:02 +0700

    add new board support stm32wba65i_dk1 refer to nucleo_wba65ri - oled display test using lvgl sample (subsys/display/lvgl) - basic led test (RGB)

--- samples/subsys/display/lvgl/src/main.c
+++ samples/subsys/display/lvgl/src/main.c
@@ -61,6 +61,8 @@ int main(void)
 	lv_obj_t *hello_world_label;
 	lv_obj_t *count_label;
 
+	LOG_INF("Test display!");
+
 	display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
 	if (!device_is_ready(display_dev)) {
 		LOG_ERR("Device not ready, aborting test");
@@ -135,7 +137,7 @@ int main(void)
 		hello_world_label = lv_label_create(lv_screen_active());
 	}
 
-	lv_label_set_text(hello_world_label, "Hello world!");
+	lv_label_set_text(hello_world_label, "Hello dunia!");
 	lv_obj_align(hello_world_label, LV_ALIGN_CENTER, 0, 0);
 
 	count_label = lv_label_create(lv_screen_active());
@@ -145,8 +147,8 @@ int main(void)
 	display_blanking_off(display_dev);
 
 	while (1) {
-		if ((count % 100) == 0U) {
-			sprintf(count_str, "%d", count/100U);
+		if ((count % 10) == 0U) {
+			sprintf(count_str, "%d", count/10U);
 			lv_label_set_text(count_label, count_str);
 		}
 		lv_timer_handler();
